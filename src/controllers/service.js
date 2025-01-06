f5f7b9debc658f84a2028cd7c3d4392d831b72b4
Anuj Singh <anuj.as828@gmail.com>
2025-01-06 20:06:57 +0530

    requirements satisfied testing left

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -4,28 +4,62 @@ const generateToken=require("../utils/generateToken.js")
 const bcrypt=require("bcrypt")
 
 const identify = asyncHandler(async (req, res) => {
-    try {
-      const {email, phone, password } = req.body;
+  try {
+      const { email: e, phone: p, password } = req.body;
 
-      //First case if email exist but not phone
+      const em = await User.findOne({ email: e });
+      const pm = await User.findOne({ phone: p });
+      let pc = em?.linkPrecedence === 'primary' ? em : pm || em;
 
-      //Second case if phone exist but not email
+      if (pc) {
+          if (pc.linkPrecedence === 'primary' && (em || pm)) {
+              pc.linkPrecedence = 'secondary';
+              await pc.save(); 
 
-      //If both not exist
-      await User.findOne({ email });
-      const details = await User.create({
-        email,
-        phone,
-        password,
-      });
-      const token = generateToken(res, details._id);
+              const newContact = await User.create({
+                  email: e,
+                  phone: p,
+                  password,
+                  linkedId: pc._id,
+                  linkPrecedence: 'primary', 
+              });
+
+              return res.status(200).json({
+                  primaryContactId: newContact._id,
+                  emails: [newContact.email],
+                  phoneNumbers: [newContact.phone],
+                  secondaryContactIds: [pc._id],
+              });
+          }
+
+          const sc = await User.create({
+              email: e,
+              phone: p,
+              password,
+              linkedId: pc._id,
+              linkPrecedence: 'secondary',
+          });
+
+          return res.status(200).json({
+              primaryContactId: pc._id,
+              emails: Array.from(new Set([pc.email, ...sc.map(c => c.email)])),
+              phoneNumbers: Array.from(new Set([pc.phone, ...sc.map(c => c.phone)])),
+              secondaryContactIds: [sc._id],
+          });
+      }
+
+      const nc = await User.create({ email: e, phone: p, password, linkedId: null, linkPrecedence: 'primary' });
       res.status(201).json({
-        token,
+          primaryContactId: nc._id,
+          emails: [nc.email],
+          phoneNumbers: [nc.phone],
+          secondaryContactIds: [],
       });
-    } catch (err) {
+  } catch (err) {
       res.status(400).json({ error: err.message });
-    }
+  }
 });
+
 
 
 module.exports={identify}
